d2dcc3216c9b0541d549c6731be9835868d56dba
jeojw <jeawookjeong@gmail.com>
2024-03-20 00:59:05 +0900

    react 앱 위치 재조정

--- opentalk/src/main/java/com/example/opentalk/OpentalkApplication.java
+++ opentalk/src/main/java/com/example/opentalk/OpentalkApplication.java
@@ -2,8 +2,10 @@ package com.example.opentalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class OpentalkApplication {
 
 	public static void main(String[] args) {
