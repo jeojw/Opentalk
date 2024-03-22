6142aa61ff8c13c9a9c67b99a0aaf8c021220f3c
jeojw <jeawookjeong@gmail.com>
2024-03-23 01:47:45 +0900

    회원가입 구현, 이메일 인증 구현

--- opentalk/src/main/java/com/example/opentalk/controller/FrontController.java
+++ opentalk/src/main/java/com/example/opentalk/controller/FrontController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/opentalk/front")
     public String FrontPage(){
-        return "FrontPage";
+        return "index";
     }
 
 }
