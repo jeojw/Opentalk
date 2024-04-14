6d33a305013a4c887c78c60f75d6741b11a0ab7f
jeojw <jeawookjeong@gmail.com>
2024-04-14 21:54:22 +0900

    css 스타일링 개선, 그리고 이미지 업로드는 일단 더미로 구현해 놨지만 좀 더 정확하게 구현해야 할 듯

--- opentalk/src/main/java/com/example/opentalk/repository/MainRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.opentalk.repository;
-
-import com.example.opentalk.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MainRepository extends JpaRepository<MemberEntity, Long> {
-}
