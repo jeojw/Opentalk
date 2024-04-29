796865da82af7f18c1b4dd9883b598eff9181aa2
jeojw <jeawookjeong@gmail.com>
2024-04-29 14:49:03 +0900

    화면 크기 조정

--- opentalk/src/main/frontend/src/jsx/room.jsx
+++ opentalk/src/main/frontend/src/jsx/room.jsx
@@ -12,18 +12,13 @@ import "../css/CustomPagination.css"
 import { useMediaQuery } from 'react-responsive';
 
 const Desktop = ({ children }) => {
-    const isDesktop = useMediaQuery({ minWidth: 1224 })
+    const isDesktop = useMediaQuery({ minWidth: 767, maxWidth:1920 })
     return isDesktop ? children : null
-}
-const Tablet = ({ children }) => {
-    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
-    return isTablet ? children : null
 }
 const Mobile = ({ children }) => {
     const isMobile = useMediaQuery({ maxWidth: 767 })
     return isMobile ? children : null
 }
-
 const RoomComponent = ({isChangeData, setIsChangeData}) => {
 
     const [roomInformation, setRoomInformation] = useState();
