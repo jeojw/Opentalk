4c43b09cdbb914b76de91586589806ba10dea5f1
jeojw <jeawookjeong@gmail.com>
2024-04-30 16:47:16 +0900

    모바일 버전 인원수 설정 수정

--- opentalk/src/main/frontend/src/jsx/changroom.jsx
+++ opentalk/src/main/frontend/src/jsx/changroom.jsx
@@ -52,7 +52,7 @@ const ChangRoomComponent = ({room_Id, stompClient, curParticipates, showModal, s
         else if (participants <= curParticipates){
             window.alert("현재 인원수보다 적게 인원을 설정할 수 없습니다.");
         }
-        else{
+        else if (participants < 3){
             window.alert("방의 인원수는 최소 3명부터 가능합니다.");
         }
     };
